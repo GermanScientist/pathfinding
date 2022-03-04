278fc16454cc0326f655b979442e3d8cd6d5b661
GermanScientist <jack_hulspas@hotmail.com>
2022-03-04 09:33:41 +0100

    Base VS program

---
+++ pathfinding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pathfinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
